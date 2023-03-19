15bee67c66fa21c362101669304f4b3f8cc358be
MOHD RAVISH <mohdravish2003@gmail.com>
2023-03-19 16:15:15 +0530

    #6 commit

--- Backend/routers/authRouter.js
+++
@@ -1,6 +0,0 @@
-const route=require('express').Router();
-const authController=require("../controllers/authController.js");
-
-route.post("/signup", authController.signupController); 
-
-module.exports=route;
