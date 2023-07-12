561ba5ae39576876aa7d33b484b2dd88a1a33c40
Syed Owais Nawaz <owaisnawaz17@gmail.com>
2023-07-12 20:02:58 +0300

    Final commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,7 @@
 const mysql = require("mysql");
 const bcrypt = require("bcryptjs");
 const { promisify } = require("util");
-// const express = require("express");
-// const app = express();
-// // const dotenv = require("dotenv").config();
-// // const cookieParser = require("cookie-parser");
 
-// app.use(express.static('public'));
 const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.DATABASE_USER,
@@ -16,37 +11,35 @@ const connection = mysql.createConnection({
 
 connection.query("create table if not exists admin(user_id int not null primary key auto_increment,user_name varchar(255),user_pass varchar(255));");
 
+connection.query("create table if not exists payment(p_name varchar(255),p_email varchar(255),p_amt varchar(255),p_branch varchar(255));");
+
 connection.query("create table if not exists loginuser(user_id int not null primary key auto_increment,user_name varchar(255),user_pass varchar(255));");
 
 connection.query("create table if not exists student(s_id int not null primary key auto_increment,student_name varchar(255),dob varchar(255),fathername varchar(255),mobileno varchar(255),gender varchar(255),email varchar(255),id_type varchar(255),id_no varchar(255),faculty varchar(255),dept varchar(255),issue varchar(255),expiry varchar(255),address_type varchar(255),nationality varchar(255),state varchar(255),district varchar(255),blk_no varchar(255),ward_no varchar(255));");
+
+connection.query("create table if not exists chat(c_id int not null primary key auto_increment,chat_message varchar(255),likes varchar(255));");
+
 
 exports.adlogin = function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
 
-    connection.query("select * from loginuser where user_name = ? and user_pass = ?", [username, password], function (error, results, fields) {
+    connection.query("select * from admin where user_name = ? and user_pass = ?", [username, password], function (error, results, fields) {
         if (results.length > 0) {
-            //   return res.sendFile(__dirname + "/dashboard.html", {
-            //   })
-            // res.redirect("/dashboard");
-            // return;
-            // console.log(results)
-                return res.render("admin-dashboard", {
+
+            return res.render("admin-dashboard", {
                 message: 'admin logged in'
-                });
+            });
         } else {
-                // res.redirect("/");
-                // return;
-                return res.render("login",{
-                  message: 'wrongg'
-              })
+
+            req.flash('error', 'Email or password is incorrect!');
+            return res.redirect("/login");
         }
         res.end();
     })
 }
 
 exports.studentregs = async function (req, res) {
-    // const { name, susername, password, passwordConfirm } = req.body;
     var rusername = req.body.rusername;
     var ruserpass = req.body.ruserpass;
     connection.query('SELECT user_name from loginuser WHERE user_name = ?', [rusername], async (err, results) => {
@@ -54,10 +47,8 @@ exports.studentregs = async function (req, res) {
             console.log(err);
         } else {
             if (results.length > 0) {
-                return res.render("student", {
-                    message: 'The email is already in use'
-                })
-
+                req.flash('error', 'The email is already in use!');
+                return res.redirect("/student");
             }
         }
 
@@ -68,33 +59,38 @@ exports.studentregs = async function (req, res) {
             if (error) {
                 console.log(error);
             } else {
-                return res.render("student", {
-                    message: 'User registered'
-                });
+                req.flash('success', 'User Registered!');
+                return res.redirect("/student");
             }
         })
     })
 }
-
 
 exports.studentlogin = async function (req, res) {
     try {
         var susername = req.body.susername;
         var suserpass = req.body.suserpass;
         if (!susername || !suserpass) {
-            return res.status(400).render("student",{
-                message: "Please Provide an email and password"
-            })
+            req.flash('error', 'Please Provide an email or password!');
+            return res.redirect("/student");
         }
         connection.query('SELECT * from loginuser WHERE user_name = ?', [susername], async (err, results) => {
             console.log(results[0]);
             if (!results || !results.length || !await bcrypt.compare(suserpass, results[0].user_pass)) {
-                return res.status(401).render("student",{
-                    message: 'Email or Password is incorrect'});
+                req.flash('error', 'Email or password is incorrect!');
+                return res.redirect("/student");
 
             } else {
-                return res.status(200).render("dashboard",{
-                    message: "Student logged in"});
+                connection.query('SELECT * from student WHERE email = ?', [susername], async (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(result);
+                        return res.render("dashboard", {
+                            data1: result
+                        });
+                    }
+                });
             }
         })
     } catch (err) {
@@ -102,7 +98,7 @@ exports.studentlogin = async function (req, res) {
     }
 }
 
-exports.registration = async function(req,res){
+exports.registration = async function (req, res) {
     var stud_name = req.body.stud_name;
     var dob = req.body.dob;
     var father_name = req.body.father_name;
@@ -123,13 +119,13 @@ exports.registration = async function(req,res){
     var ward = req.body.ward;
 
 
-    connection.query('SELECT student_name from student WHERE student_name = ?', [stud_name], async (err, results) => {
+    connection.query('SELECT * from student WHERE email = ?', [email], async (err, results) => {
         if (err) {
             console.log(err);
-        } 
+        }
         else {
             if (results.length > 0) {
-                return res.send("data exists");
+                return res.redirect("/dashboard")
             }
         }
 
@@ -138,8 +134,64 @@ exports.registration = async function(req,res){
             if (error) {
                 console.log(error);
             } else {
-                return res.render("dashboard");
+                return res.redirect("/dashboard");
             }
         })
+    })
+}
+
+exports.input = async function (req, res) {
+    var chat = req.body.chat;
+    connection.query("insert into chat(chat_message) values( ? )", [chat], async function (error, results, fields) {
+        if (error) {
+            console.log(error);
+
+        } else {
+            return res.redirect("/dashboard");
+        }
+    })
+}
+
+exports.chat = async function (req, res) {
+    connection.query('SELECT * from chat ORDER BY c_id DESC', async (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (result.length > 0) {
+                console.log(result);
+                return res.render("chats", { data: result });
+            }
+        }
+    })
+}
+
+
+
+exports.payment = async function (req, res) {
+    var p_name = req.body.p_name;
+    var p_email = req.body.p_email;
+    var p_amt = req.body.p_amt;
+    var p_branch = req.body.p_branch;
+
+    connection.query("insert into payment(p_name,p_email,p_amt,p_branch) values(? , ? , ? , ?)", [p_name, p_email, p_amt, p_branch], async function (error, results, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            return res.redirect("/dashboard");
+        }
+    })
+
+}
+
+exports.chat = async function (req, res) {
+    connection.query('SELECT * from chat ORDER BY c_id DESC', async (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (result.length > 0) {
+                console.log(result);
+                return res.render("chats", { data: result });
+            }
+        }
     })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -7,5 +7,8 @@ router.post('/adlogin', authController.adlogin);
 router.post('/studentregs', authController.studentregs);
 router.post('/studentlogin', authController.studentlogin);
 router.post('/registration', authController.registration);
+router.post('/payment', authController.payment);
+router.post('/input', authController.input);
+router.post('/chat', authController.chat);
 
 module.exports = router;
