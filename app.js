4cc116108cebe7c229edf2e44cd0e6b4fa8cb408
mohd-ravish <mohdravish2003@gmail.com>
2023-08-20 21:37:04 +0530

    app.js updated

--- app.js
+++ app.js
@@ -39,4 +39,4 @@ connection.connect((err) => {
 app.use('/', require('./routes/pages'));
 app.use('/', require('./routes/auth'));
 
-app.listen(4500);
+app.listen(4500, () => console.log("Server is Running on Port 4500"));
